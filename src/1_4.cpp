b0a6891e87f9d9238cd5b5e883a338d5dae6b2f2
Chun Meng <mengchun0120@gmail.com>
2018-02-02 07:04:25 -0500

    Initialized 1_4

---
+++ src/1_4.cpp
@@ -0,0 +1,21 @@
+#include <cassert>
+#include <iostream>
+#include <string>
+
+/*
+Write a function that given a string would print the 'expanded' version of it.
+For example a2[bc2[c]]d would print out abcccbcccd. Note: The number before the
+opening and closing square brackets is the multiplier for the characters within
+the square brackets.
+*/
+
+using namespace std;
+
+void expand(const string& pattern, string& output)
+{
+    //
+}
+
+int main(int argc, char* argv[])
+{
+}
