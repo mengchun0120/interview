420583a50e1537cd44e565531fbbdc2438fe9b1f
Chun Meng <mengchun0120@gmail.com>
2019-10-14 22:35:44 -0400

    Added flatten & weight sum of nested list

---
+++ src/flatten_nested_list_iterator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+# Given a nested ist of integers, implement an iterator to flatten it. Each
+# element is either an integer, or a list -- whose element may also be integers or
+# other lists.
+#
+# Example 1: Input: [[1,1],2,[1,1]], Output: [1,1,2,1,1]
+# Explanation: By calling next repeatedly until hasNext returns false, the order
+# of elements returned next should be [1,1,2,1,1]
+#
+# Example 2: Input: [1,[4,[6]]], Output: [1,4,6]
+
+class Iterator:
+    def __init__(self, nestedList):
+        pass
+
+    def hasNext():
+        return False
+
+    def next():
+        return None
+
+def test(nestedList):
+    pass
+
+def main():
+    return 0
+
+if __name__ == "__main__":
+    exit(main())
