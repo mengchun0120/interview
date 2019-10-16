050d3a2d3fa238db9d58078a539271800936c41d
Chun Meng <mengchun0120@gmail.com>
2019-10-15 22:43:46 -0400

    Nested list weight sum

---
+++ src/flatten_nested_list_iterator.cpp
@@ -0,0 +1,15 @@
+/*
+Given a nested ist of integers, implement an iterator to flatten it. Each
+element is either an integer, or a list -- whose element may also be integers or
+other lists.
+
+Example 1: Input: [[1,1],2,[1,1]], Output: [1,1,2,1,1]
+Explanation: By calling next repeatedly until hasNext returns false, the order
+of elements returned next should be [1,1,2,1,1]
+
+ Example 2: Input: [1,[4,[6]]], Output: [1,4,6]
+*/
+
+int main()
+{
+}
