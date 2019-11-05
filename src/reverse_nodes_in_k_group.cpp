44d103beca8b24db6a0c368575bf06a3067c200b
Chun Meng <mengchun0120@gmail.com>
2019-11-04 22:24:04 -0500

    reverse nodes in k group

--- src/reverse_nodes_in_k_group.cpp
+++ src/reverse_nodes_in_k_group.cpp
@@ -13,6 +13,7 @@ list's nodes, only nodes itself may be changed.
 
 #include <cassert>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -26,10 +27,83 @@ struct Node {
     {}
 };
 
+void print(Node *head)
+{
+    Node *n = head;
+    while(n) {
+        cout << n->val << ' ';
+        n = n->next;
+    }
+    cout << endl;
+}
+
 Node *reverseGroup(Node *head, int k)
 {
+    if(!head || k < 2)
+        return head;
+
+    Node *first = head;
+    Node *groupPrev, *groupFirst;
+
+    groupPrev = nullptr;
+    groupFirst = head;
+    while(groupFirst) {
+        Node *groupEnd = groupFirst->next;
+        for(int i = 2; i < k && groupEnd; ++i) {
+            groupEnd = groupEnd->next;
+        }
+
+        if(!groupEnd)
+            break;
+
+        Node *groupNext = groupEnd->next;
+
+        Node *prev = groupFirst;
+        Node *n = prev->next;
+        while(true) {
+            Node *next = n->next;
+            n->next = prev;
+            if(n == groupEnd)
+                break;
+            prev = n;
+            n = next;
+        }
+
+        groupFirst->next = groupNext;
+        if(groupPrev) {
+            groupPrev->next = groupEnd;
+        } else {
+            first = groupEnd;
+        }
+
+        groupPrev = groupFirst;
+        groupFirst = groupNext;
+    }
+
+    return first;
+}
+
+Node *initList(const vector<int>& v)
+{
+    if(v.size() == 0)
+        return nullptr;
+
+    Node *first = new Node(v[0]);
+    Node *prev = first;
+
+    for(unsigned int i = 1; i < v.size(); ++i) {
+        Node *n = new Node(v[i]);
+        prev->next = n;
+        prev = n;
+    }
+
+    return first;
 }
 
 int main()
 {
+    vector<int> v1{1,2,3,4,5,6,7,8};
+    Node *head1 = initList(v1);
+    Node *r1 = reverseGroup(head1, 3);
+    print(r1);
 }
